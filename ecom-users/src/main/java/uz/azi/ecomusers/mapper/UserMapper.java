d578c2924fe63602830263a3d076ac6e8fbbe700
hp_admin <shoxistarakhimova28@gmail.com>
2022-05-31 16:31:48 +0500

    added feign client

--- ecom-users/src/main/java/uz/azi/ecomusers/mapper/UserMapper.java
+++ ecom-users/src/main/java/uz/azi/ecomusers/mapper/UserMapper.java
@@ -4,7 +4,7 @@ package uz.azi.ecomusers.mapper;
 import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 import uz.azi.ecomusers.dtos.UserCreateDto;
-import uz.azi.ecomusers.dtos.UserDTo;
+import uz.azi.ecomusers.dtos.UserDto;
 import uz.azi.ecomusers.dtos.UserUpdateDto;
 import uz.azi.ecomusers.model.User;
 
@@ -14,8 +14,8 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    UserDTo toDto(User user);
-    List<UserDTo> toDTos(List<User> users);
+    UserDto toDto(User user);
+    List<UserDto> toDTos(List<User> users);
 
     User fromCreateDto(UserCreateDto dto);
 
